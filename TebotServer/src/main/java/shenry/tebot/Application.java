0a01f2cbad44e61f3badfdc369760ab243444bbb
shenry <shenry2@ya.ru>
2015-12-13 21:59:23 +0300

    TODOs added.

--- TebotServer/src/main/java/shenry/tebot/Application.java
+++ TebotServer/src/main/java/shenry/tebot/Application.java
@@ -9,6 +9,13 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan(value = "shenry.tebot")
 public class Application
 {
+    // TODO move api key to config file
+    // TODO allow multiple api keys
+    // TODO determine threads count from proc count
+    // TODO write logs to file
+    // TODO move configuration to separate class
+    // TODO use Spring IOC-container instead of explicit instance creation
+
     public static void main ( String[] args ) throws Exception {
         ApplicationContext ctx = new AnnotationConfigApplicationContext(Application.class);
         TebotServer server = new TebotServer("Api key here", ctx, 2);
